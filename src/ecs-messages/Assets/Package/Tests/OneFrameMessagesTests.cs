39e9044636bd4da3c9f7f467b7d07b5d87420b5c
Oleksandr Skorniakov <cortexdeveloper@gmail.com>
2022-08-07 22:08:35 +0300

    add tetsfor one frame buffer command

--- src/ecs-messages/Assets/Package/Tests/OneFrameMessagesTests.cs
+++ src/ecs-messages/Assets/Package/Tests/OneFrameMessagesTests.cs
@@ -1,9 +1,7 @@
 using System.Collections;
-using System.Linq;
 using CortexDeveloper.Messages.Components;
 using CortexDeveloper.Messages.Service;
 using NUnit.Framework;
-using Unity.Collections;
 using Unity.Entities;
 using UnityEngine;
 using UnityEngine.TestTools;
@@ -13,72 +11,55 @@ namespace CortexDeveloper.Tests
     public class OneFrameMessagesTests
     {
         [UnityTest]
-        public IEnumerator Post_Command_Check_Existing_WaitFrame_Check_AutoRemove()
+        public IEnumerator PostEvent_CheckForExisting_WaitFrame_CheckForAutoRemove()
         {
             // Act
-            MessageBroadcaster.PrepareCommand().Post(new TestContentData{ Value = 1 });
+            MessageBroadcaster.PrepareEvent().Post(new TestContentData{ Value = 123 });
             yield return null;
 
             // Assert
             EntityQuery query = TestsUtils.GetQuery<TestContentData>();
             TestContentData component = TestsUtils.GetComponentFromFirst<TestContentData>(query);
             bool wasPosted = query.CalculateEntityCount() > 0 &&
-                                   TestsUtils.FirstEntityHasComponent<MessageContextCommandTag>(query) &&
-                                   component.Value == 1;
-            
+                                   TestsUtils.FirstEntityHasComponent<MessageTag>(query) &&
+                                   TestsUtils.FirstEntityHasComponent<MessageContextEventTag>(query) &&
+                                   TestsUtils.FirstEntityHasComponent<MessageLifetimeOneFrameTag>(query) &&
+                                   component.Value == 123;
+
             yield return null;
 
             bool wasAutoRemoved = !TestsUtils.IsExist<TestContentData>();
             
             Assert.IsTrue(wasPosted && wasAutoRemoved);
-            
-            Assert.IsTrue(wasPosted);
         }
         
         [UnityTest]
-        public IEnumerator Post_Event_Check_Existing_WaitFrame_Check_AutoRemove()
+        public IEnumerator PostBufferCommand_CheckForExisting_WaitFrame_CheckForAutoRemove()
         {
             // Act
-            MessageBroadcaster.PrepareEvent().Post(new TestContentData{ Value = 2 });
+            MessageBroadcaster.PrepareCommand().PostBuffer(
+                new TestContentBufferData { Value = 123 },
+                new TestContentBufferData { Value = 456 },
+                new TestContentBufferData { Value = 789 });
+            
             yield return null;
 
             // Assert
-            EntityQuery query = TestsUtils.GetQuery<TestContentData>();
-            TestContentData component = TestsUtils.GetComponentFromFirst<TestContentData>(query);
+            EntityQuery query = TestsUtils.GetBufferQuery<TestContentBufferData>();
+            DynamicBuffer<TestContentBufferData> buffer = TestsUtils.GetBufferFromFirst<TestContentBufferData>(query);
             bool wasPosted = query.CalculateEntityCount() > 0 &&
-                                   TestsUtils.FirstEntityHasComponent<MessageContextEventTag>(query) &&
-                                   component.Value == 2;
+                             TestsUtils.FirstEntityHasComponent<MessageTag>(query) &&
+                             TestsUtils.FirstEntityHasComponent<MessageContextCommandTag>(query) &&
+                             TestsUtils.FirstEntityHasComponent<MessageLifetimeOneFrameTag>(query) &&
+                             buffer[0].Value == 123 &&
+                             buffer[1].Value == 456 &&
+                             buffer[2].Value == 789;
 
             yield return null;
 
-            bool wasAutoRemoved = !TestsUtils.IsExist<TestContentData>();
+            bool wasAutoRemoved = !TestsUtils.IsBufferExist<TestContentBufferData>();
             
             Assert.IsTrue(wasPosted && wasAutoRemoved);
         }
-        
-        // [UnityTest]
-        // public IEnumerator Post_OneFrame_Event_Buffer_Then_Check_For_Existing_WaitFrame_Check_For_AutoRemove()
-        // {
-        //     // Act
-        //     MessageBroadcaster.PrepareEvent().PostBuffer(
-        //         new TestContentBufferData { Value = 123 },
-        //         new TestContentBufferData { Value = 456 },
-        //         new TestContentBufferData { Value = 789 });
-        //     
-        //     yield return null;
-        //
-        //     // Assert
-        //     EntityQuery query = TestsUtils.GetQuery<>();
-        //     TestContentData component = TestsUtils.GetComponentFromFirst<TestContentData>(query);
-        //     bool wasPosted = query.CalculateEntityCount() > 0 &&
-        //                      TestsUtils.FirstEntityHasComponent<MessageContextEventTag>(query) &&
-        //                      component.Value == 2;
-        //
-        //     yield return null;
-        //
-        //     bool wasAutoRemoved = !TestsUtils.IsExist<TestContentData>();
-        //     
-        //     Assert.IsTrue(wasPosted && wasAutoRemoved);
-        // }
     }
 }
--- src/ecs-messages/Assets/Package/Tests/TestsUtils.cs
+++ src/ecs-messages/Assets/Package/Tests/TestsUtils.cs
@@ -20,6 +20,20 @@ namespace CortexDeveloper.Tests
             return query;
         }
         
+        internal static EntityQuery GetBufferQuery<T>() where T : struct, IBufferElementData
+        {
+            EntityQueryDescBuilder descBuilder = new(Allocator.Temp);
+            descBuilder.AddAny(new ComponentType(typeof(T)));
+            descBuilder.FinalizeQuery();
+
+            EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+            EntityQuery query = entityManager.CreateEntityQuery(descBuilder);
+
+            descBuilder.Dispose();
+
+            return query;
+        }
+        
         internal static T GetComponentFromFirst<T>(EntityQuery query) where T : struct, IComponentData
         {
             EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
@@ -27,6 +41,15 @@ namespace CortexDeveloper.Tests
             T component = entityManager.GetComponentData<T>(entity);
 
             return component;
+        }
+        
+        internal static DynamicBuffer<T> GetBufferFromFirst<T>(EntityQuery query) where T : struct, IBufferElementData
+        {
+            EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+            Entity entity = GetFirst(query);
+            DynamicBuffer<T> buffer = entityManager.GetBuffer<T>(entity);
+
+            return buffer;
         }
 
         internal static Entity GetFirst(EntityQuery query)
@@ -42,12 +65,15 @@ namespace CortexDeveloper.Tests
         internal static bool IsExist<T>() where T : struct, IComponentData => 
             GetQuery<T>().CalculateEntityCount() > 0;
         
+        internal static bool IsBufferExist<T>() where T : struct, IBufferElementData => 
+            GetBufferQuery<T>().CalculateEntityCount() > 0;
+        
         internal static bool FirstEntityHasComponent<T>(EntityQuery query) where T : struct, IComponentData
         {
             EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
             Entity entity = GetFirst(query);
             
-            return entityManager.HasComponent<TestContentData>(entity);
+            return entityManager.HasComponent<T>(entity);
         }
     }
 }
