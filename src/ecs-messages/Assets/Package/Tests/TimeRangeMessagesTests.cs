e5fd898e0566d1e19905d4b9788307f31d23052a
Oleksandr Skorniakov <cortexdeveloper@gmail.com>
2022-08-15 23:16:36 +0300

    add attached messages unit tests

--- src/ecs-messages/Assets/Package/Tests/TimeRangeMessagesTests.cs
+++ src/ecs-messages/Assets/Package/Tests/TimeRangeMessagesTests.cs
@@ -123,5 +123,54 @@ namespace CortexDeveloper.Tests
             
             Assert.IsTrue(wasPosted && existedAfterOneSecondPassed && wasAutoRemoved);
         }
+        
+        [UnityTest]
+        public IEnumerator PostUniqueAttachedBufferCommand_PostUniqueAttachedBufferCommand_WaitFrame_CheckOnlyOneExist_CheckForAutoRemove()
+        {
+            // Arrange
+            EntityManager entityManager = World.DefaultGameObjectInjectionWorld
+                .GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>()
+                .EntityManager;
+
+            Entity firstEntity = entityManager.CreateEntity();
+            Entity secondEntity = entityManager.CreateEntity();
+            
+            // Act
+            MessageBroadcaster.PrepareEvent().AsUnique().AttachedTo(firstEntity).AliveForTime(2f).PostBuffer(
+                new TestContentBufferData { Value = 123 },
+                new TestContentBufferData { Value = 456 });
+            
+            MessageBroadcaster.PrepareEvent().AsUnique().AttachedTo(secondEntity).AliveForTime(2f).PostBuffer(
+                new TestContentBufferData { Value = 123 },
+                new TestContentBufferData { Value = 456 });
+
+            yield return null;
+            
+            // Assert
+            EntityQuery query = TestsUtils.GetQuery<MessageTag>();
+            EntityQuery attachedQuery = TestsUtils.GetBufferQuery<TestContentBufferData>();
+            DynamicBuffer<TestContentBufferData> buffer = TestsUtils.GetBufferFromFirst<TestContentBufferData>(attachedQuery);
+            bool wasPosted = query.CalculateEntityCount() == 1 &&
+                             attachedQuery.CalculateEntityCount() == 1 &&
+                             TestsUtils.FirstEntityHasComponent<MessageTag>(query) &&
+                             TestsUtils.FirstEntityHasComponent<MessageContextEventTag>(query) &&
+                             TestsUtils.FirstEntityHasComponent<MessageLifetimeTimeRange>(query) &&
+                             TestsUtils.FirstEntityHasComponent<AttachedMessageContent>(query) &&
+                             buffer[0].Value == 123 &&
+                             buffer[1].Value == 456;
+
+            yield return new WaitForSeconds(1f);
+            
+            bool existedAfterOneSecondPassed = TestsUtils.GetBufferQuery<TestContentBufferData>().CalculateEntityCount() == 1;
+
+            yield return new WaitForSeconds(1f);
+
+            bool wasAutoRemoved = !TestsUtils.IsBufferExist<TestContentBufferData>();
+            
+            entityManager.DestroyEntity(firstEntity);
+            entityManager.DestroyEntity(secondEntity);
+            
+            Assert.IsTrue(wasPosted && existedAfterOneSecondPassed && wasAutoRemoved);
+        }
     }
 }
--- src/ecs-messages/Assets/Package/Tests/UnlimitedMessagesTests.cs
+++ src/ecs-messages/Assets/Package/Tests/UnlimitedMessagesTests.cs
@@ -11,7 +11,7 @@ namespace CortexDeveloper.Tests
     public class UnlimitedMessagesTests
     {
         [UnityTest]
-        public IEnumerator PostUnlimitedEvent_CheckForExisting_ManuallyRemove_WaitTwoFrames_CheckForRemove()
+        public IEnumerator PostEvent_CheckForExisting_ManuallyRemove_WaitTwoFrames_CheckForRemove()
         {
             // Act
             MessageBroadcaster.PrepareEvent().AliveForUnlimitedTime().Post(new TestContentData{ Value = 123 });
@@ -37,7 +37,7 @@ namespace CortexDeveloper.Tests
         }
         
         [UnityTest]
-        public IEnumerator PostUnlimitedBufferCommand_CheckForExisting_ManuallyRemove_WaitTwoFrames_CheckForRemove()
+        public IEnumerator PostBufferCommand_CheckForExisting_ManuallyRemove_WaitTwoFrames_CheckForRemove()
         {
             // Act
             MessageBroadcaster.PrepareCommand().AliveForUnlimitedTime().PostBuffer(
@@ -64,6 +64,44 @@ namespace CortexDeveloper.Tests
             yield return null;
 
             bool wasRemoved = !TestsUtils.IsBufferExist<TestContentBufferData>();
+            
+            Assert.IsTrue(wasPosted && wasRemoved);
+        }
+        
+        [UnityTest]
+        public IEnumerator PostAttachedEvent_CheckForExisting_ManuallyRemove_WaitTwoFrames_CheckForRemove()
+        {
+            // Arrange
+            EntityManager entityManager = World.DefaultGameObjectInjectionWorld
+                .GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>()
+                .EntityManager;
+
+            Entity entity = entityManager.CreateEntity();
+
+            // Act
+            MessageBroadcaster.PrepareEvent().AttachedTo(entity).AliveForUnlimitedTime().Post(new TestContentData{ Value = 123 });
+            yield return null;
+
+            // Assert
+            EntityQuery query = TestsUtils.GetQuery<MessageTag>();
+            EntityQuery attachedQuery = TestsUtils.GetQuery<TestContentData>();
+            TestContentData component = TestsUtils.GetComponentFromFirst<TestContentData>(attachedQuery);
+            bool wasPosted = query.CalculateEntityCount() == 1 &&
+                             attachedQuery.CalculateEntityCount() == 1 &&
+                             TestsUtils.FirstEntityHasComponent<MessageTag>(query) &&
+                             TestsUtils.FirstEntityHasComponent<MessageContextEventTag>(query) &&
+                             TestsUtils.FirstEntityHasComponent<MessageLifetimeUnlimitedTag>(query) &&
+                             TestsUtils.FirstEntityHasComponent<AttachedMessageContent>(query) &&
+                             component.Value == 123;
+
+            MessageBroadcaster.RemoveCommonWithLifetime(MessageLifetime.Unlimited);
+            
+            yield return null;
+            yield return null;
+
+            bool wasRemoved = !TestsUtils.IsExist<TestContentData>();
+
+            entityManager.DestroyEntity(entity);
             
             Assert.IsTrue(wasPosted && wasRemoved);
         }
