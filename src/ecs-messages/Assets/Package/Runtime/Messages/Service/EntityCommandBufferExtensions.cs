82b98971a7f7c5f66d97db3c960f333d74f04022
CortexDeveloper <cortexdeveloper@gmail.com>
2022-09-27 22:54:22 +0300

    add remove api as ecb extensions

--- src/ecs-messages/Assets/Package/Runtime/Messages/Service/EntityCommandBufferExtensions.cs
+++ src/ecs-messages/Assets/Package/Runtime/Messages/Service/EntityCommandBufferExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Unity.Entities;
+﻿using CortexDeveloper.Messages.Components;
+using Unity.Entities;
 
 namespace CortexDeveloper.Messages.Service
 {
@@ -17,5 +18,14 @@ namespace CortexDeveloper.Messages.Service
                 Ecb = ecb,
                 Context = MessageContext.Command
             };
+
+        public static void RemoveMessage(this EntityCommandBuffer ecb, EntityManager entityManager, Entity entity) =>
+            MessageBroadcaster.RemoveMessage(ecb, entityManager, entity);
+
+        public static void RemoveAllMessages(this EntityCommandBuffer ecb) =>
+            MessageBroadcaster.RemoveAllMessages(ecb);
+
+        public static void RemoveAllMessagesWith<T>(this EntityCommandBuffer ecb) where T : struct, IComponentData, IMessageComponent =>
+            MessageBroadcaster.RemoveAllMessagesWith<T>(ecb);
     }
 }
