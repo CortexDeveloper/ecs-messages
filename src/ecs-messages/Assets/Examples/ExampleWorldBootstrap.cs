c471ea338eae970f9d536de1308b27fe01efa1cb
Oleksandr Skorniakov <cortexdeveloper@gmail.com>
2022-11-24 10:59:53 +0200

    add world time update system to example world

--- src/ecs-messages/Assets/Examples/ExampleWorldBootstrap.cs
+++ src/ecs-messages/Assets/Examples/ExampleWorldBootstrap.cs
@@ -7,22 +7,41 @@ namespace CortexDeveloper.Examples
         public bool Initialize(string defaultWorldName)
         {
             World exampleWorld = new("Example World");
-
-            exampleWorld.GetOrCreateSystem<InitializationSystemGroup>();
-            SimulationSystemGroup simulationSystemGroup = exampleWorld.GetOrCreateSystem<SimulationSystemGroup>();
-            exampleWorld.GetOrCreateSystem<PresentationSystemGroup>();
-
-            BeginSimulationEntityCommandBufferSystem beginSimulationEntityCommandBufferSystem =
-                exampleWorld.GetOrCreateSystem<BeginSimulationEntityCommandBufferSystem>();
-            EndSimulationEntityCommandBufferSystem endSimulationEntityCommandBufferSystem =
-                exampleWorld.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
             
-            simulationSystemGroup.AddSystemToUpdateList(beginSimulationEntityCommandBufferSystem);
-            simulationSystemGroup.AddSystemToUpdateList(endSimulationEntityCommandBufferSystem);
+            CreateInitializationGroup(exampleWorld);
+            CreateSimulationGroup(exampleWorld);
+            CreatePresentationGroup(exampleWorld);
             
             ScriptBehaviourUpdateOrder.AppendWorldToCurrentPlayerLoop(exampleWorld);
 
             return false;
+        }
+
+        private void CreateInitializationGroup(World world)
+        {
+            InitializationSystemGroup initGroup = world.GetOrCreateSystem<InitializationSystemGroup>();
+
+            UpdateWorldTimeSystem updateWorldTimeSystem = world.GetOrCreateSystem<UpdateWorldTimeSystem>();
+            
+            initGroup.AddSystemToUpdateList(updateWorldTimeSystem);
+        }
+        
+        private void CreateSimulationGroup(World world)
+        {
+            SimulationSystemGroup simGroup = world.GetOrCreateSystem<SimulationSystemGroup>();
+            
+            BeginSimulationEntityCommandBufferSystem beginSimulationEntityCommandBufferSystem =
+                world.GetOrCreateSystem<BeginSimulationEntityCommandBufferSystem>();
+            EndSimulationEntityCommandBufferSystem endSimulationEntityCommandBufferSystem =
+                world.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+            
+            simGroup.AddSystemToUpdateList(beginSimulationEntityCommandBufferSystem);
+            simGroup.AddSystemToUpdateList(endSimulationEntityCommandBufferSystem);
+        }
+        
+        private void CreatePresentationGroup(World world)
+        {
+            world.GetOrCreateSystem<PresentationSystemGroup>();
         }
     }
 }
