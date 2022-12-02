a26bf74770094f229fab4e06da96aa7adf819a8d
Oleksandr Skorniakov <cortexdeveloper@gmail.com>
2022-12-03 01:18:34 +0200

    update test cases

--- src/ecs-messages/Assets/Package/Tests/OneFrameMessagesTests.cs
+++ src/ecs-messages/Assets/Package/Tests/OneFrameMessagesTests.cs
@@ -1,76 +1,81 @@
-// using System.Collections;
-// using CortexDeveloper.Messages.Components.Meta;
-// using CortexDeveloper.Messages.Service;
-// using CortexDeveloper.Tests.Components;
-// using NUnit.Framework;
-// using Unity.Entities;
-// using UnityEngine.TestTools;
-//
-// namespace CortexDeveloper.Tests
-// {
-//     public class OneFrameMessagesTests
-//     {
-//         [UnitySetUp]
-//         public IEnumerator SetUp()
-//         {
-//             yield return new EnterPlayMode();
-//             
-//             TestUtils.InitializeTestWorld();
-//         }
-//         
-//         [UnityTearDown]
-//         public IEnumerator TearDown()
-//         {
-//             MessageBroadcaster.Dispose();
-//             
-//             yield return new ExitPlayMode();
-//         }
-//         
-//         [UnityTest]
-//         public IEnumerator PostEvent_WaitFrame_CheckForExisting_WaitFrame_CheckForAutoRemove()
-//         {
-//             // Act
-//             MessageBroadcaster.PrepareEvent(TestUtils.GetEcbSystem().CreateCommandBuffer()).AliveForOneFrame().Post(new TestContentData{ Value = 123 });
-//             yield return null;
-//
-//             // Assert
-//             EntityQuery query = TestUtils.GetQuery<TestContentData>();
-//             TestContentData component = TestUtils.GetComponentFromFirstEntity<TestContentData>(query);
-//             bool wasPosted = query.CalculateEntityCount() == 1 &&
-//                                    TestUtils.FirstEntityHasComponent<MessageTag>(query) &&
-//                                    TestUtils.FirstEntityHasComponent<MessageContextEventTag>(query) &&
-//                                    TestUtils.FirstEntityHasComponent<MessageLifetimeOneFrameTag>(query) &&
-//                                    component.Value == 123;
-//
-//             yield return null;
-//
-//             bool wasAutoRemoved = !TestUtils.IsEntityWithComponentExist<TestContentData>();
-//             
-//             Assert.IsTrue(wasPosted && wasAutoRemoved);
-//         }
-//         
-//         [UnityTest]
-//         public IEnumerator PostCommand_WaitFrame_CheckForExisting_WaitFrame_CheckForAutoRemove()
-//         {
-//             // Act
-//             MessageBroadcaster.PrepareCommand(TestUtils.GetEcbSystem().CreateCommandBuffer()).AliveForOneFrame().Post(new TestContentData { Value = 123 });
-//             
-//             yield return null;
-//
-//             // Assert
-//             EntityQuery query = TestUtils.GetQuery<TestContentData>();
-//             TestContentData component = TestUtils.GetComponentFromFirstEntity<TestContentData>(query);
-//             bool wasPosted = query.CalculateEntityCount() == 1 &&
-//                              TestUtils.FirstEntityHasComponent<MessageTag>(query) &&
-//                              TestUtils.FirstEntityHasComponent<MessageContextCommandTag>(query) &&
-//                              TestUtils.FirstEntityHasComponent<MessageLifetimeOneFrameTag>(query) &&
-//                              component.Value == 123;
-//
-//             yield return null;
-//
-//             bool wasAutoRemoved = !TestUtils.IsEntityWithComponentExist<TestContentData>();
-//             
-//             Assert.IsTrue(wasPosted && wasAutoRemoved);
-//         }
-//     }
-// }
+using System.Collections;
+using CortexDeveloper.Messages.Components.Meta;
+using CortexDeveloper.Messages.Service;
+using CortexDeveloper.Tests.Components;
+using NUnit.Framework;
+using Unity.Entities;
+using UnityEngine.TestTools;
+
+namespace CortexDeveloper.Tests
+{
+    public class OneFrameMessagesTests
+    {
+        [UnitySetUp]
+        public IEnumerator SetUp()
+        {
+            yield return new EnterPlayMode();
+            
+            TestUtils.InitializeTestWorld();
+        }
+        
+        [UnityTearDown]
+        public IEnumerator TearDown()
+        {
+            MessageBroadcaster.Dispose();
+            
+            yield return new ExitPlayMode();
+        }
+        
+        [UnityTest]
+        public IEnumerator PostEvent_WaitFrame_CheckForExisting_WaitFrame_CheckForAutoRemove()
+        {
+            // Act
+            MessageBroadcaster
+                .PrepareMessage()
+                .AliveForOneFrame()
+                .Post(TestUtils.GetEcbSystem().CreateCommandBuffer(), new TestContentData { Value = 123 });
+            
+            yield return null;
+
+            // Assert
+            EntityQuery query = TestUtils.GetQuery<TestContentData>();
+            TestContentData component = TestUtils.GetComponentFromFirstEntity<TestContentData>(query);
+            bool wasPosted = query.CalculateEntityCount() == 1 &&
+                                   TestUtils.FirstEntityHasComponent<MessageTag>(query) &&
+                                   TestUtils.FirstEntityHasComponent<MessageLifetimeOneFrameTag>(query) &&
+                                   component.Value == 123;
+
+            yield return null;
+
+            bool wasAutoRemoved = !TestUtils.IsEntityWithComponentExist<TestContentData>();
+            
+            Assert.IsTrue(wasPosted && wasAutoRemoved);
+        }
+        
+        [UnityTest]
+        public IEnumerator PostCommand_WaitFrame_CheckForExisting_WaitFrame_CheckForAutoRemove()
+        {
+            // Act
+            MessageBroadcaster
+                .PrepareMessage()
+                .AliveForOneFrame()
+                .Post(TestUtils.GetEcbSystem().CreateCommandBuffer(), new TestContentData { Value = 123 });
+            
+            yield return null;
+
+            // Assert
+            EntityQuery query = TestUtils.GetQuery<TestContentData>();
+            TestContentData component = TestUtils.GetComponentFromFirstEntity<TestContentData>(query);
+            bool wasPosted = query.CalculateEntityCount() == 1 &&
+                             TestUtils.FirstEntityHasComponent<MessageTag>(query) &&
+                             TestUtils.FirstEntityHasComponent<MessageLifetimeOneFrameTag>(query) &&
+                             component.Value == 123;
+
+            yield return null;
+
+            bool wasAutoRemoved = !TestUtils.IsEntityWithComponentExist<TestContentData>();
+            
+            Assert.IsTrue(wasPosted && wasAutoRemoved);
+        }
+    }
+}
--- src/ecs-messages/Assets/Package/Tests/TimeRangeMessagesTests.cs
+++ src/ecs-messages/Assets/Package/Tests/TimeRangeMessagesTests.cs
@@ -1,106 +1,112 @@
-// using System.Collections;
-// using CortexDeveloper.Messages.Components.Meta;
-// using CortexDeveloper.Messages.Service;
-// using CortexDeveloper.Tests.Components;
-// using NUnit.Framework;
-// using Unity.Entities;
-// using UnityEngine;
-// using UnityEngine.TestTools;
-//
-// namespace CortexDeveloper.Tests
-// {
-//     public class TimeRangeMessagesTests
-//     {
-//         [UnitySetUp]
-//         public IEnumerator SetUp()
-//         {
-//             yield return new EnterPlayMode();
-//             
-//             TestUtils.InitializeTestWorld();
-//         }
-//         
-//         [UnityTearDown]
-//         public IEnumerator TearDown()
-//         {            
-//             MessageBroadcaster.Dispose();
-//
-//             yield return new ExitPlayMode();
-//         }
-//         
-//         [UnityTest]
-//         public IEnumerator PostOneSecondEvent_WaitFrame_CheckForExisting_WaitOneSecond_CheckForAutoRemove()
-//         {
-//             // Act
-//             MessageBroadcaster.PrepareEvent(TestUtils.GetEcbSystem().CreateCommandBuffer()).AliveForSeconds(1f).Post(new TestContentData{ Value = 123 });
-//             
-//             yield return null;
-//
-//             // Assert
-//             EntityQuery query = TestUtils.GetQuery<TestContentData>();
-//             TestContentData component = TestUtils.GetComponentFromFirstEntity<TestContentData>(query);
-//             bool wasPosted = query.CalculateEntityCount() == 1 &&
-//                                    TestUtils.FirstEntityHasComponent<MessageTag>(query) &&
-//                                    TestUtils.FirstEntityHasComponent<MessageContextEventTag>(query) &&
-//                                    TestUtils.FirstEntityHasComponent<MessageLifetimeTimeRange>(query) &&
-//                                    component.Value == 123;
-//
-//             yield return new WaitForSeconds(1f);
-//
-//             bool wasAutoRemoved = !TestUtils.IsEntityWithComponentExist<TestContentData>();
-//             
-//             Assert.IsTrue(wasPosted && wasAutoRemoved);
-//         }
-//         
-//         [UnityTest]
-//         public IEnumerator PostTwoSecondsCommand_WaitOneSecond_CheckForExisting_WaitOneSecond_CheckForAutoRemove()
-//         {
-//             // Act
-//             MessageBroadcaster.PrepareCommand(TestUtils.GetEcbSystem().CreateCommandBuffer()).AliveForSeconds(2f).Post(new TestContentData{ Value = 123 });
-//             
-//             yield return null;
-//
-//             // Assert
-//             EntityQuery query = TestUtils.GetQuery<TestContentData>();
-//             TestContentData component = TestUtils.GetComponentFromFirstEntity<TestContentData>(query);
-//             bool wasPosted = query.CalculateEntityCount() == 1 &&
-//                              TestUtils.FirstEntityHasComponent<MessageTag>(query) &&
-//                              TestUtils.FirstEntityHasComponent<MessageContextCommandTag>(query) &&
-//                              TestUtils.FirstEntityHasComponent<MessageLifetimeTimeRange>(query) &&
-//                              component.Value == 123;
-//
-//             yield return new WaitForSeconds(1f);
-//             
-//             bool existedAfterOneSecondPassed = TestUtils.GetQuery<TestContentData>().CalculateEntityCount() == 1;
-//
-//             yield return new WaitForSeconds(1f);
-//
-//             bool wasAutoRemoved = !TestUtils.IsEntityWithComponentExist<TestContentData>();
-//             
-//             Assert.IsTrue(wasPosted && existedAfterOneSecondPassed && wasAutoRemoved);
-//         }
-//         
-//         [UnityTest]
-//         public IEnumerator PostCommand_CheckForExisting_WaitOneSecond_CheckForAutoRemove()
-//         {
-//             // Act
-//             MessageBroadcaster.PrepareCommand(TestUtils.GetEcbSystem().CreateCommandBuffer()).AliveForSeconds(1f).Post(new TestContentData { Value = 123 });
-//             
-//             yield return null;
-//
-//             // Assert
-//             EntityQuery query = TestUtils.GetQuery<TestContentData>();
-//             TestContentData component = TestUtils.GetComponentFromFirstEntity<TestContentData>(query);
-//             bool wasPosted = query.CalculateEntityCount() == 1 &&
-//                              TestUtils.FirstEntityHasComponent<MessageTag>(query) &&
-//                              TestUtils.FirstEntityHasComponent<MessageContextCommandTag>(query) &&
-//                              TestUtils.FirstEntityHasComponent<MessageLifetimeTimeRange>(query) &&
-//                              component.Value == 123;
-//
-//             yield return new WaitForSeconds(1f);
-//
-//             bool wasAutoRemoved = !TestUtils.IsEntityWithComponentExist<TestContentData>();
-//             
-//             Assert.IsTrue(wasPosted && wasAutoRemoved);
-//         }
-//     }
-// }
+using System.Collections;
+using CortexDeveloper.Messages.Components.Meta;
+using CortexDeveloper.Messages.Service;
+using CortexDeveloper.Tests.Components;
+using NUnit.Framework;
+using Unity.Entities;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace CortexDeveloper.Tests
+{
+    public class TimeRangeMessagesTests
+    {
+        [UnitySetUp]
+        public IEnumerator SetUp()
+        {
+            yield return new EnterPlayMode();
+            
+            TestUtils.InitializeTestWorld();
+        }
+        
+        [UnityTearDown]
+        public IEnumerator TearDown()
+        {            
+            MessageBroadcaster.Dispose();
+
+            yield return new ExitPlayMode();
+        }
+        
+        [UnityTest]
+        public IEnumerator PostOneSecondEvent_WaitFrame_CheckForExisting_WaitOneSecond_CheckForAutoRemove()
+        {
+            // Act
+            MessageBroadcaster
+                .PrepareMessage()
+                .AliveForSeconds(1f)
+                .Post(TestUtils.GetEcbSystem().CreateCommandBuffer(), new TestContentData{ Value = 123 });
+            
+            yield return null;
+
+            // Assert
+            EntityQuery query = TestUtils.GetQuery<TestContentData>();
+            TestContentData component = TestUtils.GetComponentFromFirstEntity<TestContentData>(query);
+            bool wasPosted = query.CalculateEntityCount() == 1 &&
+                                   TestUtils.FirstEntityHasComponent<MessageTag>(query) &&
+                                   TestUtils.FirstEntityHasComponent<MessageLifetimeTimeRange>(query) &&
+                                   component.Value == 123;
+
+            yield return new WaitForSeconds(1f);
+
+            bool wasAutoRemoved = !TestUtils.IsEntityWithComponentExist<TestContentData>();
+            
+            Assert.IsTrue(wasPosted && wasAutoRemoved);
+        }
+        
+        [UnityTest]
+        public IEnumerator PostTwoSecondsCommand_WaitOneSecond_CheckForExisting_WaitOneSecond_CheckForAutoRemove()
+        {
+            // Act
+            MessageBroadcaster
+                .PrepareMessage()
+                .AliveForSeconds(2f)
+                .Post(TestUtils.GetEcbSystem().CreateCommandBuffer(), new TestContentData{ Value = 123 });
+            
+            yield return null;
+
+            // Assert
+            EntityQuery query = TestUtils.GetQuery<TestContentData>();
+            TestContentData component = TestUtils.GetComponentFromFirstEntity<TestContentData>(query);
+            bool wasPosted = query.CalculateEntityCount() == 1 &&
+                             TestUtils.FirstEntityHasComponent<MessageTag>(query) &&
+                             TestUtils.FirstEntityHasComponent<MessageLifetimeTimeRange>(query) &&
+                             component.Value == 123;
+
+            yield return new WaitForSeconds(1f);
+            
+            bool existedAfterOneSecondPassed = TestUtils.GetQuery<TestContentData>().CalculateEntityCount() == 1;
+
+            yield return new WaitForSeconds(1f);
+
+            bool wasAutoRemoved = !TestUtils.IsEntityWithComponentExist<TestContentData>();
+            
+            Assert.IsTrue(wasPosted && existedAfterOneSecondPassed && wasAutoRemoved);
+        }
+        
+        [UnityTest]
+        public IEnumerator PostCommand_CheckForExisting_WaitOneSecond_CheckForAutoRemove()
+        {
+            // Act
+            MessageBroadcaster
+                .PrepareMessage()
+                .AliveForSeconds(1f)
+                .Post(TestUtils.GetEcbSystem().CreateCommandBuffer(), new TestContentData { Value = 123 });
+            
+            yield return null;
+
+            // Assert
+            EntityQuery query = TestUtils.GetQuery<TestContentData>();
+            TestContentData component = TestUtils.GetComponentFromFirstEntity<TestContentData>(query);
+            bool wasPosted = query.CalculateEntityCount() == 1 &&
+                             TestUtils.FirstEntityHasComponent<MessageTag>(query) &&
+                             TestUtils.FirstEntityHasComponent<MessageLifetimeTimeRange>(query) &&
+                             component.Value == 123;
+
+            yield return new WaitForSeconds(1f);
+
+            bool wasAutoRemoved = !TestUtils.IsEntityWithComponentExist<TestContentData>();
+            
+            Assert.IsTrue(wasPosted && wasAutoRemoved);
+        }
+    }
+}
