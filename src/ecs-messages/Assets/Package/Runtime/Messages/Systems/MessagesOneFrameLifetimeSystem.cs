5d8c67be2f18199c3d905e094fb370b43b48a353
Oleksandr Skorniakov <cortexdeveloper@gmail.com>
2022-12-04 14:21:22 +0200

    remove uneccessary create method in one frame system

--- src/ecs-messages/Assets/Package/Runtime/Messages/Systems/MessagesOneFrameLifetimeSystem.cs
+++ src/ecs-messages/Assets/Package/Runtime/Messages/Systems/MessagesOneFrameLifetimeSystem.cs
@@ -8,13 +8,6 @@ namespace CortexDeveloper.Messages.Systems
     [DisableAutoCreation]
     public partial class MessagesOneFrameLifetimeSystem : SystemBase
     {
-        protected override void OnCreate()
-        {
-            base.OnCreate();
-
-            RequireForUpdate(GetEntityQuery(ComponentType.ReadOnly<MessageTag>(), ComponentType.ReadOnly<MessageLifetimeOneFrameTag>()));
-        }
-        
         protected override void OnUpdate()
         {
             EntityQuery query = GetEntityQuery(ComponentType.ReadOnly<MessageTag>(), ComponentType.ReadOnly<MessageLifetimeOneFrameTag>());
