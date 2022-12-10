7e09f073d78c980a1167c133ff3f528eeaa193fd
Oleksandr Skorniakov <cortexdeveloper@gmail.com>
2022-12-10 18:48:32 +0200

    move post api to builder

--- src/ecs-messages/Assets/Package/Runtime/Messages/Service/MessageBuilder.cs
+++ src/ecs-messages/Assets/Package/Runtime/Messages/Service/MessageBuilder.cs
@@ -1,4 +1,8 @@
+using CortexDeveloper.Messages.Components;
+using CortexDeveloper.Messages.Components.Meta;
+using CortexDeveloper.Messages.Systems;
 using Unity.Collections;
+using Unity.Entities;
 
 namespace CortexDeveloper.Messages.Service
 {
@@ -28,6 +32,58 @@ namespace CortexDeveloper.Messages.Service
             Lifetime = MessageLifetime.Unlimited;
 
             return this;
+        }
+        
+        public void Post<T>(EntityCommandBuffer ecb, T component) where T : unmanaged, IComponentData, IMessageComponent
+        {
+            Entity messageEntity = ecb.CreateEntity();
+
+#if UNITY_EDITOR
+            ecb.SetName(messageEntity, Name);
+            ecb.AddComponent(messageEntity, new MessageEditorData
+            {
+                Id = MessageBroadcaster.RandomGen.Data.NextInt(0, int.MaxValue),
+                CreationTime = MessagesDateTimeSystem.TimeAsString.Data
+            });
+#endif
+            
+            ecb.AddComponent(messageEntity, new MessageTag());
+
+            if (Lifetime == MessageLifetime.OneFrame)
+                ecb.AddComponent(messageEntity, new MessageLifetimeOneFrameTag());
+            else if (Lifetime == MessageLifetime.TimeRange)
+                ecb.AddComponent(messageEntity, new MessageLifetimeTimeRange { LifetimeLeft = LifetimeSeconds });
+            else if (Lifetime == MessageLifetime.Unlimited)
+                ecb.AddComponent(messageEntity, new MessageLifetimeUnlimitedTag());
+            
+            ecb.AddComponent(messageEntity, component);
+        }
+        
+        public Entity PostImmediate<T>(EntityManager entityManager, T component) where T : unmanaged, IComponentData, IMessageComponent
+        {
+            Entity messageEntity = entityManager.CreateEntity();
+
+#if UNITY_EDITOR
+            entityManager.SetName(messageEntity, Name);
+            entityManager.AddComponentData(messageEntity, new MessageEditorData
+            {
+                Id = MessageBroadcaster.RandomGen.Data.NextInt(0, int.MaxValue),
+                CreationTime = MessagesDateTimeSystem.TimeAsString.Data
+            });
+#endif
+            
+            entityManager.AddComponentData(messageEntity, new MessageTag());
+            
+            if (Lifetime == MessageLifetime.OneFrame)
+                entityManager.AddComponentData(messageEntity, new MessageLifetimeOneFrameTag());
+            else if (Lifetime == MessageLifetime.TimeRange)
+                entityManager.AddComponentData(messageEntity, new MessageLifetimeTimeRange { LifetimeLeft = LifetimeSeconds });
+            else if (Lifetime == MessageLifetime.Unlimited)
+                entityManager.AddComponentData(messageEntity, new MessageLifetimeUnlimitedTag());
+
+            entityManager.AddComponentData(messageEntity, component);
+
+            return messageEntity;
         }
     }
 }
