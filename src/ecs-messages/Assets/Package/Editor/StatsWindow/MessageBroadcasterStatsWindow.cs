07e4f637abe79ffee13183feb7a51b55d3a3c81d
CortexDeveloper <cortexdeveloper@gmail.com>
2023-09-20 21:58:56 +0300

    add world dropdown list elements update

--- src/ecs-messages/Assets/Package/Editor/StatsWindow/MessageBroadcasterStatsWindow.cs
+++ src/ecs-messages/Assets/Package/Editor/StatsWindow/MessageBroadcasterStatsWindow.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using System.Linq;
+using CortexDeveloper.ECSMessages.Service;
+using Unity.Entities;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -7,7 +11,14 @@ namespace CortexDeveloper.ECSMessages.Editor.StatsWindow
     public class MessageBroadcasterStatsWindow : EditorWindow
     {
         private const string UxmlPath = "Assets/Package/Editor/StatsWindow/MessageBroadcasterStatsWindow.uxml";
+        private const string WorldDropdown = "WorldDropdown";
 
+        private List<string> _worldsList = new();
+        private int _selectedWorld;
+        private World SelectedWorld => World.All.GetWorldWithName(_worldsList[_selectedWorld]);
+
+        private DropdownField _worldDropdown;
+        
         [MenuItem("ECSMessages/MessageBroadcasterStatsWindow")]
         public static void ShowExample()
         {
@@ -23,6 +34,37 @@ namespace CortexDeveloper.ECSMessages.Editor.StatsWindow
             VisualElement buildTree = visualTree.Instantiate();
         
             root.Add(buildTree);
+            
+            _worldDropdown = rootVisualElement.Q<DropdownField>(WorldDropdown);
+            _worldDropdown.RegisterValueChangedCallback(evt => Debug.Log(evt.newValue));
+        }
+
+        private void OnInspectorUpdate()
+        {
+            UpdateWorldDropdownList();
+        }
+
+        private void UpdateWorldDropdownList()
+        {
+            _worldsList.Clear();
+
+            for (int i = 0; i < World.All.Count; i++)
+                _worldsList.Add(default);
+
+            int j = 0;
+            foreach (World world in World.All)
+            {
+                if (j < World.All.Count)
+                    _worldsList[j] = world.Name;
+                else
+                    break;
+
+                j++;
+            }
+
+            _worldsList = _worldsList.Where(w => !w.Contains("LoadingWorld")).ToList();
+
+            _worldDropdown.choices = _worldsList;
         }
     }
 }
